36d9ceca3d8c27316465981e86c39bf3f592f17f
Sathya Tadinada <sathyav.tadinada@gmail.com>
2024-12-07 16:36:44 -0700

    Added React-Scan

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -1,5 +1,8 @@
 import { type MDXComponents } from 'mdx/types'
 import Image, { type ImageProps } from 'next/image'
+import { Monitoring } from 'react-scan/dist/core/monitor/params/next'
+
+<Monitoring apiKey="eEuHxeELyg0axtDlHCyikXzi9i83UDle" url="https://monitoring.react-scan.com/api/v1/ingest" />
 
 export function useMDXComponents(components: MDXComponents) {
   return {
