73ecf63ba72908405613b812de83b93a2d085bf3
Sathya Tadinada <sathyav.tadinada@gmail.com>
2025-08-10 23:53:37 -0600

    Refactored code so that all the Prose styling is inside tailwind.config.ts

--- src/components/Prose.tsx
+++ src/components/Prose.tsx
@@ -8,26 +8,7 @@ export function Prose({
     <div
       {...props}
       className={clsx(
-        // base typography
-        'prose dark:prose-invert',
-
-        // link text color (fixed)
-        'prose-a:text-blue-500 dark:prose-a:text-blue-400',
-
-        // base underline: thin + faded
-        'prose-a:underline underline-offset-2 decoration-[1px]',
-        'prose-a:decoration-blue-500/30 dark:prose-a:decoration-blue-400/30',
-
-        // smooth per-link hover changes
-        '[&_a]:transition-[text-decoration-thickness,text-decoration-color, color]',
-        '[&_a]:duration-200 [&_a]:ease-out',
-        '[&_a:hover]:decoration-blue-700 dark:[&_a:hover]:decoration-blue-400',
-        '[&_a:hover]:text-blue-700 dark:[&_a:hover]:text-blue-400',
-
-        // backup: override typography vars
-        '[--tw-prose-links:theme(colors.blue.500)]',
-        'dark:[--tw-prose-invert-links:theme(colors.blue.400)]',
-
+        'prose dark:prose-invert mt-8 text-zinc-600 dark:text-zinc-400 max-w-none',
         className
       )}
     />
