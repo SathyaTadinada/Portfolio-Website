a22e1d08bbdde6ef94d7ba237c9f5ba2a1f19fde
Sathya Tadinada <sathyav.tadinada@gmail.com>
2026-02-04 15:44:46 -0700

    Modified rest of the code to be static content instead of dynamic

--- src/app/blog/layout.tsx
+++ src/app/blog/layout.tsx
@@ -1,7 +1,10 @@
+export const dynamic = 'force-static'
+export const revalidate = false
+
 export default function BlogLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
-  return <>{children}</>
+  return children
 }
