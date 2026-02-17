9ef1011a4d76a953db398b2606c0a7f232fa01ca
Sathya Tadinada <sathyav.tadinada@gmail.com>
2026-02-17 02:26:39 -0700

    Added icons to Contacts page too

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -11,19 +11,21 @@ import logoMastodon from '@/images/logos/mastodon.svg'
 import logoMail from '@/images/logos/mail.svg'
 import logoLinkedIn from '@/images/logos/linkedin.svg'
 
+import { Link, ChevronRight } from 'lucide-react'
+
 function DashedDivider({ padding }: { padding: number }) {
   if (padding === 0) {
     return (
       <ul className="grid grid-cols-1 sm:grid-cols-1 lg:grid-cols-1">
-        <div className="border-dashed border-t border-zinc-100 dark:border-zinc-700/40 mt-8"></div>
+        <div className="mt-8 border-t border-dashed border-zinc-100 dark:border-zinc-700/40"></div>
       </ul>
     )
   } else if (padding === 4) {
     return (
       <ul className="grid grid-cols-4 sm:grid-cols-4 lg:grid-cols-4">
         <div></div>
-        <div className="border-dashed border-t border-zinc-100 dark:border-zinc-700/40 mt-4"></div>
-        <div className="border-dashed border-t border-zinc-100 dark:border-zinc-700/40 mt-4"></div>
+        <div className="mt-4 border-t border-dashed border-zinc-100 dark:border-zinc-700/40"></div>
+        <div className="mt-4 border-t border-dashed border-zinc-100 dark:border-zinc-700/40"></div>
         <div></div>
       </ul>
     )
@@ -31,8 +33,8 @@ function DashedDivider({ padding }: { padding: number }) {
     return (
       <ul className="grid grid-cols-4 sm:grid-cols-4 lg:grid-cols-4">
         <div></div>
-        <div className="border-dashed border-t border-zinc-100 dark:border-zinc-700/40 mt-6"></div>
-        <div className="border-dashed border-t border-zinc-100 dark:border-zinc-700/40 mt-6"></div>
+        <div className="mt-6 border-t border-dashed border-zinc-100 dark:border-zinc-700/40"></div>
+        <div className="mt-6 border-t border-dashed border-zinc-100 dark:border-zinc-700/40"></div>
         <div></div>
       </ul>
     )
@@ -40,8 +42,8 @@ function DashedDivider({ padding }: { padding: number }) {
     return (
       <ul className="grid grid-cols-4 sm:grid-cols-4 lg:grid-cols-4">
         <div></div>
-        <div className="border-dashed border-t border-zinc-100 dark:border-zinc-700/40 mt-8"></div>
-        <div className="border-dashed border-t border-zinc-100 dark:border-zinc-700/40 mt-8"></div>
+        <div className="mt-8 border-t border-dashed border-zinc-100 dark:border-zinc-700/40"></div>
+        <div className="mt-8 border-t border-dashed border-zinc-100 dark:border-zinc-700/40"></div>
         <div></div>
       </ul>
     )
@@ -49,8 +51,8 @@ function DashedDivider({ padding }: { padding: number }) {
     return (
       <ul className="grid grid-cols-4 sm:grid-cols-4 lg:grid-cols-4">
         <div></div>
-        <div className="border-dashed border-t border-zinc-100 dark:border-zinc-700/40 mt-12"></div>
-        <div className="border-dashed border-t border-zinc-100 dark:border-zinc-700/40 mt-12"></div>
+        <div className="mt-12 border-t border-dashed border-zinc-100 dark:border-zinc-700/40"></div>
+        <div className="mt-12 border-t border-dashed border-zinc-100 dark:border-zinc-700/40"></div>
         <div></div>
       </ul>
     )
@@ -70,7 +72,13 @@ function LinkIcon(props: React.ComponentPropsWithoutRef<'svg'>) {
 
 function ChevronRightIcon(props: React.ComponentPropsWithoutRef<'svg'>) {
   return (
-    <svg fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" {...props}>
+    <svg
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth="1.5"
+      stroke="currentColor"
+      {...props}
+    >
       <path
         d="m 7.425 4.05 l 6.75 6.75 l -6.75 6.75"
         strokeLinejoin="round"
@@ -85,14 +93,22 @@ const primaryContact = [
     name: 'Email',
     description:
       'This is the best way to reach me, I check my email every day and respond promptly.',
-    link: { href: 'mailto:sathya@tadinada.com', label: 'sathya@tadinada.com', hover: 'Send email' },
+    link: {
+      href: 'mailto:sathya@tadinada.com',
+      label: 'sathya@tadinada.com',
+      hover: 'Send email',
+    },
     logo: logoMail,
   },
   {
     name: 'LinkedIn',
     description:
       'Connect with me on LinkedIn, where I share my professional updates.',
-    link: { href: 'https://linkedin.com/in/sathya-tadinada/', label: 'sathya-tadinada', hover: 'Visit LinkedIn' },
+    link: {
+      href: 'https://linkedin.com/in/sathya-tadinada/',
+      label: 'sathya-tadinada',
+      hover: 'Visit LinkedIn',
+    },
     logo: logoLinkedIn,
   },
 ]
@@ -102,28 +118,43 @@ const otherProfiles = [
     name: 'GitHub',
     description:
       "I'm an active open-source contributor and developer, check out my projects on GitHub.",
-    link: { href: 'https://github.com/SathyaTadinada', label: 'SathyaTadinada', hover: 'Visit GitHub' },
+    link: {
+      href: 'https://github.com/SathyaTadinada',
+      label: 'SathyaTadinada',
+      hover: 'Visit GitHub',
+    },
     logo: logoGithub,
   },
   {
     name: 'X',
     description:
       'Follow me here for my latest thoughts and updates, I tend to post here more frequently.',
-    link: { href: 'https://x.com/@SathyaTadinada', label: '@SathyaTadinada', hover: 'Visit X' },
+    link: {
+      href: 'https://x.com/@SathyaTadinada',
+      label: '@SathyaTadinada',
+      hover: 'Visit X',
+    },
     logo: logoX,
   },
   {
     name: 'Instagram',
-    description:
-      'Check out my profile for pictures of my travels and hobbies.',
-    link: { href: 'https://instagram.com/sathya.tadinada/', label: '@sathya.tadinada', hover: 'Visit Instagram' },
+    description: 'Check out my profile for pictures of my travels and hobbies.',
+    link: {
+      href: 'https://instagram.com/sathya.tadinada/',
+      label: '@sathya.tadinada',
+      hover: 'Visit Instagram',
+    },
     logo: logoInstagram,
   },
   {
     name: 'Mastodon',
     description:
       'I cross-post all my thoughts here, follow for all my latest updates.',
-    link: { href: 'https://mastodon.social/@tadinada', label: '@tadinada', hover: 'Visit Mastodon' },
+    link: {
+      href: 'https://mastodon.social/@tadinada',
+      label: '@tadinada',
+      hover: 'Visit Mastodon',
+    },
     logo: logoMastodon,
   },
 ]
@@ -154,7 +185,7 @@ export default function Contact() {
           >
             {primaryContact.map((contact) => (
               <Card as="li" key={contact.name}>
-                <div className="relative z-10 flex h-12 w-12 items-center justify-center rounded-full bg-white shadow-md shadow-zinc-800/5 ring-1 ring-zinc-900/5 dark:border dark:border-zinc-700/50 dark:bg-zinc-800 dark:ring-0">
+                <div className="relative z-10 flex h-12 w-12 items-center justify-center rounded-full bg-white shadow-md ring-1 shadow-zinc-800/5 ring-zinc-900/5 dark:border dark:border-zinc-700/50 dark:bg-zinc-800 dark:ring-0">
                   <Image
                     src={contact.logo}
                     alt=""
@@ -163,15 +194,33 @@ export default function Contact() {
                   />
                 </div>
                 <h2 className="mt-6 text-base font-semibold text-zinc-800 dark:text-zinc-100">
-                  <Card.Link href={contact.link.href} target='_blank' rel='noreferrer'>{contact.name}</Card.Link>
+                  <Card.Link
+                    href={contact.link.href}
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    {contact.name}
+                  </Card.Link>
                 </h2>
                 <Card.Description>{contact.description}</Card.Description>
-                <p className="relative z-10 mt-6 flex text-sm font-medium text-zinc-400 transition group-hover:text-blue-500 dark:text-zinc-200">
-                  <ChevronRightIcon className="block group-hover:hidden h-7 w-7 flex-auto" />
-                  <LinkIcon className="hidden group-hover:block h-7 w-7 flex-auto" />
+                <p className="relative z-10 mt-6 flex items-center gap-2 text-sm font-medium text-zinc-400 transition group-hover:text-blue-500 dark:text-zinc-200">
+                  <ChevronRight
+                    aria-hidden="true"
+                    strokeWidth={1.5}
+                    className="h-4 w-4 shrink-0 translate-y-[0.5px] group-hover:hidden"
+                  />
+                  <Link
+                    aria-hidden="true"
+                    strokeWidth={1.5}
+                    className="hidden h-4 w-4 shrink-0 translate-y-[0.5px] group-hover:block"
+                  />
 
-                  <span className="block group-hover:hidden ml-2">{contact.link.label}</span>
-                  <span className="hidden group-hover:block ml-2">{contact.link.hover}</span>
+                  <span className="leading-none group-hover:hidden">
+                    {contact.link.label}
+                  </span>
+                  <span className="hidden leading-none group-hover:block">
+                    {contact.link.hover}
+                  </span>
                 </p>
               </Card>
             ))}
@@ -193,7 +242,7 @@ export default function Contact() {
           >
             {otherProfiles.map((contact) => (
               <Card as="li" key={contact.name}>
-                <div className="relative z-10 flex h-12 w-12 items-center justify-center rounded-full bg-white shadow-md shadow-zinc-800/5 ring-1 ring-zinc-900/5 dark:border dark:border-zinc-700/50 dark:bg-zinc-800 dark:ring-0">
+                <div className="relative z-10 flex h-12 w-12 items-center justify-center rounded-full bg-white shadow-md ring-1 shadow-zinc-800/5 ring-zinc-900/5 dark:border dark:border-zinc-700/50 dark:bg-zinc-800 dark:ring-0">
                   <Image
                     src={contact.logo}
                     alt=""
@@ -202,15 +251,33 @@ export default function Contact() {
                   />
                 </div>
                 <h2 className="mt-6 text-base font-semibold text-zinc-800 dark:text-zinc-100">
-                  <Card.Link href={contact.link.href} target='_blank' rel='noreferrer'>{contact.name}</Card.Link>
+                  <Card.Link
+                    href={contact.link.href}
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    {contact.name}
+                  </Card.Link>
                 </h2>
                 <Card.Description>{contact.description}</Card.Description>
-                <p className="relative z-10 mt-6 flex text-sm font-medium text-zinc-400 transition group-hover:text-blue-500 dark:text-zinc-200">
-                  <ChevronRightIcon className="block group-hover:hidden h-7 w-7 flex-auto" />
-                  <LinkIcon className="hidden group-hover:block h-7 w-7 flex-auto" />
+                <p className="relative z-10 mt-6 flex items-center gap-2 text-sm font-medium text-zinc-400 transition group-hover:text-blue-500 dark:text-zinc-200">
+                  <ChevronRight
+                    aria-hidden="true"
+                    strokeWidth={1.5}
+                    className="h-4 w-4 shrink-0 translate-y-[0.5px] group-hover:hidden"
+                  />
+                  <Link
+                    aria-hidden="true"
+                    strokeWidth={1.5}
+                    className="hidden h-4 w-4 shrink-0 translate-y-[0.5px] group-hover:block"
+                  />
 
-                  <span className="block group-hover:hidden ml-2">{contact.link.label}</span>
-                  <span className="hidden group-hover:block ml-2">{contact.link.hover}</span>
+                  <span className="leading-none group-hover:hidden">
+                    {contact.link.label}
+                  </span>
+                  <span className="hidden leading-none group-hover:block">
+                    {contact.link.hover}
+                  </span>
                 </p>
               </Card>
             ))}
